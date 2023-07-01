57586c342e138a519f1a46857c914c88e456508c
ddobrin <ddobrin@google.com>
2023-07-01 16:58:45 -0400

    Added more tests

--- services/quotes/src/test/java/com/example/quotes/QuotesApplicationNetworkFailuresTests.java
+++ services/quotes/src/test/java/com/example/quotes/QuotesApplicationNetworkFailuresTests.java
@@ -4,11 +4,18 @@ import com.example.quotes.domain.QuoteRepository;
 import com.example.quotes.domain.QuoteService;
 import eu.rekawek.toxiproxy.Proxy;
 import eu.rekawek.toxiproxy.ToxiproxyClient;
+import eu.rekawek.toxiproxy.model.ToxicDirection;
+import eu.rekawek.toxiproxy.model.toxic.Latency;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+import org.junit.Ignore;
 import org.junit.Rule;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +30,8 @@ import org.testcontainers.containers.ToxiproxyContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Testcontainers
 @DataJpaTest
@@ -39,8 +48,7 @@ class QuotesApplicationNetworkFailuresTests {
 	private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.3-alpine")
 			.withNetwork(network).withNetworkAliases("postgres");
 
-
-
+	@Autowired
 	private QuoteRepository quoteRepository;
 	private QuoteService quoteService;
 
@@ -78,12 +86,74 @@ class QuotesApplicationNetworkFailuresTests {
 	}
 
 	@Test
-	@DisplayName("A random quote is returned")
+	@DisplayName("The normal test, no Toxi proxy")
 	void testRandomQuotes() {
-	  // var quote = this.quoteRepository.findRandomQuote();
 	  var quote = this.quoteService.findRandomQuote();
 	  assertThat(quote).isNotNull();
 	}
 
+	@Test
+	@DisplayName("Test with latency, no timeout")
+	void withLatency() throws IOException{
+		postgresqlProxy.toxics().latency("postgresql-latency", ToxicDirection.DOWNSTREAM, 1600).setJitter(100);
 
+		var quote = this.quoteService.findRandomQuote();
+		assertThat(quote).isNotNull();
+	}
+
+	@Test
+	@DisplayName("Test with latency, timeout encountered")
+	void withLatencyandTimeout() throws IOException{
+		postgresqlProxy.toxics().latency("postgresql-latency", ToxicDirection.DOWNSTREAM, 1600).setJitter(100);
+
+		try {
+			assertTimeout(Duration.ofSeconds(1), () -> {
+				this.quoteService.findRandomQuote();
+			});
+		}catch (AssertionFailedError e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Test
+	void withLatencyWithRetries() throws IOException {
+		Latency latency = postgresqlProxy.toxics().latency("postgresql-latency", ToxicDirection.DOWNSTREAM, 1600)
+				.setJitter(100);
+
+		// var quote = this.quoteService.findRandomQuote().;
+		// assertThat(quote).isNotNull();
+
+	}
+
+	@Ignore
+	@Test
+	void withToxiProxyConnectionDown() throws IOException {
+		postgresqlProxy.toxics().bandwidth("postgres-cut-connection-downstream", ToxicDirection.DOWNSTREAM, 0);
+		postgresqlProxy.toxics().bandwidth("postgres-cut-connection-upstream", ToxicDirection.UPSTREAM, 0);
+		//
+		// assertThat(
+		// 		catchThrowable(() -> {
+		// 			var quote = this.quoteService.findRandomQuote();
+		// 			assertThat(quote).isNotNull();
+		// 		})
+		// ).as("calls fail with no connection").isInstanceOf(Throwable.class);
+
+		try {
+			assertTimeout(Duration.ofSeconds(1), () -> {
+				var quote = this.quoteService.findRandomQuote();
+				assertThat(quote).isNotNull();
+			});
+		}catch (AssertionFailedError e){
+			System.out.println(e.getMessage());
+		}
+
+		// StepVerifier.create(this.serverlessServicesRepository.findAll().timeout(Duration.ofSeconds(5)))
+		// 		.verifyErrorSatisfies(throwable -> assertThat(throwable).isInstanceOf(TimeoutException.class));
+
+		postgresqlProxy.toxics().get("postgres-cut-connection-downstream").remove();
+		postgresqlProxy.toxics().get("postgres-cut-connection-upstream").remove();
+
+		var quote = this.quoteService.findRandomQuote();
+		assertThat(quote).isNotNull();
+	}
 }
